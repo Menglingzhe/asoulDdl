aae9dec0689975d87f5650c7b0365967ca832f0c
JimiTiao <56677601+Menglingzhe@users.noreply.github.com>
2022-05-12 19:03:35 +0800

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import {
-  createApp
-} from 'vue'
-import App from './App.vue'
-// import VueAxios from 'vue-axios'
-// import api from './assets/config/config';
-// app.config.globalProperties.$api = api;
-createApp(App).mount('#app')
